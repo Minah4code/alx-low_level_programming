faab5349f787c3b9ed76a80d39eeeb093ffcdc4b
Minah4code <aminahmuhammed5@gmail.com>
2023-02-17 15:04:41 -0800

    0x01. C - Variables, if, else, while

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main block 
+ * Description: prints all single digit number of base 10
+ * strating from 0, followed by a new line
+ * Return: 0 (Success)
+ */
+int main (void)
+{
+	char 
