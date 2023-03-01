59c3fb36a84b006f5815d8b672a8d71af7b8988a
Minah4code <aminahmuhammed5@gmail.com>
2023-03-01 12:08:18 -0800

    Update

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -10,21 +10,19 @@ char *rot13(char *str)
 {
 	int i;
 	int j;
-	char data1[] =
-"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-	char datarot[] =
-"NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
-	for (i = 0; s[i] != '\0', i++)
+	char data1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char datarot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	for (i = 0; str[i] != '\0'; i++)
 	{
 		for (j = 0; j < 52; j++)
 		{
-			if (s[i] == data1[j])
+			if (str[i] == data1[j])
 			{
-				s[i] = datarot[j];
+				str[i] = datarot[j];
 				break;
 			}
 		}
 
 	}
-	return (s);
+	return (str);
 }
