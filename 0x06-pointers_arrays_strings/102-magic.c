359417151cc1ed4580ed515501046b9451e9d1cd
Minah4code <aminahmuhammed5@gmail.com>
2023-03-04 13:26:36 -0800

    102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
-#include "main.h"
-int main(void)
+
+/*
+ * This program demonstrates the use of pointers in C.
+ * The main function declares an integer variable called 'n',
+ * an integer array called 'a' with a size of 5, and an integer
+ * pointer variable called 'p'. It assigns the value 1024 to
+ * the third element of the 'a' array, and assigns the memory
+ * address of the 'n' variable to the 'p' pointer variable.
+ * It then assigns the value 98 to the memory location pointed
+ * to by the 'p' pointer variable, which is the memory location
+ * of the 'n' variable. Finally, it prints the value of the
+ * third element of the 'a' array, which is 1024.
+ */
+int (void)
 {
 int n;
 int a[5];
@@ -8,16 +20,9 @@ int *p;
 
 a[2] = 1024;
 p = &n;
-/*
-* write your line of code here...
-* Remember:
-* - you are not allowed to use a
-* - you are not allowed to modify p
-* - only one statement
-* - you are not allowed to code anything else than this line of code
-*/
 *p = 98;
-/* ...so that this prints 98\n */
+
 printf("a[2] = %d\n", a[2]);
+
 return (0);
 }
