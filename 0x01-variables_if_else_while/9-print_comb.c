2e880fa62a103aecb53029623990d630fe87d68f
Minah4code <aminahmuhammed5@gmail.com>
2023-04-24 07:11:38 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,6 +6,20 @@
  * strating from 0, followed by a new line
  * Return: 0 (Success)
  */
-int main (void)
+int main(void)
 {
-	char 
+    int i;
+
+    for (i = 0; i < 10; i++)
+    {
+        putchar(i + '0');
+        if (i != 9)
+        {
+            putchar(',');
+            putchar(' ');
+        }
+    }
+    putchar('\n');
+
+    return (0);
+}
