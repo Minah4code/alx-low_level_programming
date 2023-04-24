6c4df0b97e0ffeb58a5a887eae9d19a3c6b81117
Minah4code <aminahmuhammed5@gmail.com>
2023-04-24 06:40:47 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,19 +1,12 @@
 #include <stdio.h>
-#include <stdlib.h>
-/**
- * main - main block
- * Description: Get a random it with 5
- * Return: 0
- */
+
 int main(void)
 {
-	char c = 'a';
-	
-	while (c <= '2')
-			{
-			putchar(c);
-			c++;
-			}
-			putchar('\n');
-			return (0);
+  char c;
+  
+  for (c = 'a'; c <= 'z'; c++)
+    putchar(c);
+
+  putchar('\n');
+  return (0);
 }
