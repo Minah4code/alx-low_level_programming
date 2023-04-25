61be96a0059da04f96da74b90687b1c13ed06f01
Minah4code <aminahmuhammed5@gmail.com>
2023-04-25 11:15:14 -0700

    Update

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -2,15 +2,18 @@
 
 int main(void)
 {
-int limit = 1024;
-int sum = 0;
-for (int i = 1; i < limit; i++)
-{
-if (i % 3 == 0 || i % 5 == 0) 
-{
-sum += i;
-}
-}
-printf("%d\n", sum);
-return 0;
+	int limit = 1024;
+	int sum = 0;
+	
+	for (int i = 1; i < limit; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+		{
+			sum += i;
+		}
+	}
+	
+	printf("%d\n", sum);
+	
+	return 0;
 }
