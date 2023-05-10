378f78b7d9b8d3e0cb7d503b83f9eddbfb7775cb
Minah4code <aminahmuhammed5@gmail.com>
2023-05-10 08:05:21 -0700

    6-is_prime_number.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -3,44 +3,40 @@
 
 int _sqrt(int n, int i)
 
-/**
- * sqrt_helper - Recursive helper function to find the square root
- * @n: The number to find the square root of
- * @start: The starting point for searching the square root
- * @end: The ending point for searching the square root
- *
- * Return: The square root of the number, or -1 if it does not have a
- * natural square root
- */
 
-int sqrt_helper(int n, int start, int end)
+/**
+ * sqrt_check - Recursive helper function to find the square root
+ * @n: The number to find the square root of
+ * @start: The starting point for the search
+ * @end: The ending point for the search
+ *
+ * Return: The square root of n if found, otherwise -1
+ */
+int sqrt_check(int n, int start, int end)
 {
-	if (start > end)
-		return (-1);
-	int mid = (start + end) / 2;
-	if (mid * mid == n)
-	{
-		return (mid);
-	}
-	else if (mid * mid < n)
-	{
-		return (sqrt_helper(n, mid + 1, end));
-	}
-	else
-		return (sqrt_helper(n, start, mid - 1));
+    if (start > end)
+        return -1;
+
+    int mid = (start + end) / 2;
+    long long square = (long long)mid * mid;
+
+    if (square == n)
+        return mid;
+    else if (square > n)
+        return sqrt_check(n, start, mid - 1);
+    else
+        return sqrt_check(n, mid + 1, end);
 }
 
 /**
  * _sqrt_recursion - Returns the natural square root of a number
  * @n: The number to find the square root of
  *
- * Return: The square root of the number, or -1 if it does not have a
- * natural square root
+ * Return: The square root of n if found, otherwise -1
  */
-
 int _sqrt_recursion(int n)
 {
-	if (n < 0)
-		return (-1);
-	return (sqrt_helper(n, 0, n));
+    if (n < 0)
+        return -1;
+    return sqrt_check(n, 0, n);
 }
